f69c2a58884788ba1751bf519e41bbf9d3f29a71
VaibhavGavali1 <vaibhavgavali4677@gmail.com>
2023-05-24 19:00:07 +0530

    Control Structure

---
+++ Control Structure/TernaryOperator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TernaryOperator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,max;
+        System.out.println("Enter 2 number");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        max=a>b?a:b;
+        if(a>b){
+            max=a;
+        }else{
+            max=b;
+        }
+        System.out.println("Max value is "+max);
+        }
+}
